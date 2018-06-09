fa9210fde12e5c3d243859ae60ab3d7b2275192b
cocobino <coolbino@naver.com>
2018-06-10 01:22:27 +0900

    16394번:홍익대학교

---
+++ 백준/BOJ 16394 홍익대학교/Main.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Main {
+    
+   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+   static Scanner sc = new Scanner(System.in);
+   static StringTokenizer st;
+  
+   
+   
+    public static void main(String[] args) throws IOException {
+    
+     
+    	bw.write(String.valueOf(Integer.parseInt(br.readLine())-1946));
+    	bw.flush();
+    	
+	}// main
+    
+}//class
