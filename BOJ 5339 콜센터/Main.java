ab5cd112d9ada6deb2cf851e636a8113d804a68f
cocobino <coolbino@naver.com>
2018-07-24 22:54:28 +0900

    ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

---
+++ BOJ 5339 콜센터/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Vector;
+ 
+ 
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    	 bw.write(String.valueOf("     /~\\"+"\n"));
+    	 bw.write(String.valueOf("    ( oo|"+"\n"));
+    	 bw.write(String.valueOf("    _\\=/_"+"\n"));
+    	 bw.write(String.valueOf("   /  _  \\"+"\n"));
+    	 bw.write(String.valueOf("  //|/.\\|\\\\"+"\n"));
+    	 bw.write(String.valueOf(" ||  \\ /  ||"+"\n"));
+    	 bw.write(String.valueOf("============"+"\n"));
+    	 bw.write(String.valueOf("|          |"+"\n"));
+    	 bw.write(String.valueOf("|          |"+"\n"));
+    	 bw.write(String.valueOf("|          |"));
+    	 bw.flush();
+    	 
+ 	}
+ }
+
+
+    	 bw.flush();
+    }
+
+   
+    }
+
