d0172f05aac0a197355dce714e5a8ee59eb2fa85
cocobino <coolbino@naver.com>
2018-08-01 14:45:39 +0900

    진법 : 16진수

---
+++ BOJ 1550 16진수/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String a = br.readLine();
+		
+		int rst = Integer.parseInt(a, 16);
+		bw.write(String.valueOf(rst));
+		bw.flush();
+	}
+}
