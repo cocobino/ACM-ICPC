2f7f76f20ef0695a7f1c6ca112454f4df4456a16
cocobino <coolbino@naver.com>
2018-08-22 18:21:59 +0900

    5338번 : 마이크로소프트 로고

---
+++ BOJ 5338 마이크로소프트 로고/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigDecimal;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+ 
+	 
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	 
+	
+		bw.write(String.valueOf("       _.-;;-._"+"\n"));
+		bw.write(String.valueOf("'-..-'|   ||   |"+"\n"));
+		bw.write(String.valueOf("'-..-'|_.-;;-._|"+"\n"));
+		bw.write(String.valueOf("'-..-'|   ||   |"+"\n"));
+		bw.write(String.valueOf("'-..-'|_.-''-._|"+"\n"));
+		bw.flush();
+	
+	}
+}
