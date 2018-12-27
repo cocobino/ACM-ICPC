6565e228857038efb0974f0fd9e3096e45cabf67
cocobino <coolbino@naver.com>
2018-12-27 17:13:57 +0900

    9086번:문자열

---
+++ 백준/BOJ 9086 문자열/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static StringTokenizer st;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 
+	static int n;
+ 	static boolean visit[];
+ 
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	int test_case = Integer.parseInt(br.readLine());
+	
+	for(int T=0; T<test_case; T++) {
+		String a = br.readLine();
+		
+		 
+		bw.write(a.charAt(0)+(a.charAt(a.length()-1)+"\n"));
+		bw.flush();
+		
+		}//test_case
+		
+	}//main
+
+	 
+	
+ 
+}//class   
