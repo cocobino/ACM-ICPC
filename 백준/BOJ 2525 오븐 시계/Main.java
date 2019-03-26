01fdf81128ca9d34499a0d74661ba52f7acd6e1d
cocobino <coolbino@naver.com>
2019-03-26 09:44:45 +0900

    2525 : 오븐시계

---
+++ 백준/BOJ 2525 오븐 시계/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int r, c, n;
+	static int map[][];
+	static boolean readvis[][];
+
+	static int dx[] = { 1, 0, -1, 0 }; // го , ©Л, ╩С, аб
+	static int dy[] = { 0, 1, 0, -1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int cos = Integer.parseInt(br.readLine());
+		 
+		m+=cos;
+		h+=m/60;
+		m%=60;
+		h%=24;
+		
+		System.out.println(h +" "+ m);
+		
+	}// main
+}// class
